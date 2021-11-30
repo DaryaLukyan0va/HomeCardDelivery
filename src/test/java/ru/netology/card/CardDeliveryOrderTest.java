2bfefd3ab2cabf844475ae59ab4de916f15c50a4
Daria Lukyanova <luk.dyr@yandex.ru>
2021-11-30 16:08:28 +0300

    break test on text

--- src/test/java/ru/netology/card/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/card/CardDeliveryOrderTest.java
@@ -21,14 +21,14 @@ public class CardDeliveryOrderTest {
         Configuration.headless = true;
         open("http://localhost:9999/");
         $("[data-test-id=city] [class='input__control']").setValue("Белгород");
-        String planningDate = LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String planningDate = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='date'] [class='input__control']").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='date'] [class='input__control']").setValue(planningDate);
         $("[data-test-id=name] [class='input__control']").setValue("Лукьянова Дарья");
         $("[data-test-id=phone] [class='input__control']").setValue("+79999999999");
         $("[data-test-id=agreement] [class='checkbox__box']").click();
         $("[class='button__content']").click();
-        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(withText("Ура!")).shouldBe(visible, Duration.ofSeconds(15));
         $(".notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
     }
 
